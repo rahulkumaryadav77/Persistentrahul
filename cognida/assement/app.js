3fb384ccfcc424274d58f9bac15644c7c1670edc
Rahul kumar yadav <62733143+rahulkumaryadav77@users.noreply.github.com>
2023-01-27 19:26:48 +0530

    Add files via upload

---
+++ cognida/assement/app.js
@@ -0,0 +1,14 @@
+const express  = require('express');
+const app = express();
+
+
+app.get('/',(req,res) => {
+
+    res.send("Hello ");
+
+})
+
+app.listen(3000, () => {
+
+    console.log('serving on port 3000');
+})
